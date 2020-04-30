4e27917a5e1d2f2404ea2125e1fa37521af6e5fb
Millie <47400469+Minnie-Million@users.noreply.github.com>
2020-04-30 15:59:05 -0700

    Food Collection Complete

--- ants/Ant.java
+++ ants/Ant.java
@@ -9,12 +9,18 @@ import greenfoot.*;
  */
 public class Ant extends Creature
 {
+    private boolean carryingFood = false;
+    private GreenfootImage image1;
+    private GreenfootImage image2;
+    
     /**
      * Create an ant with a given home hill. The initial speed is zero (not moving).
      */
     public Ant(AntHill home)
     {
         setHomeHill(home);
+        image1 = getImage();
+        image2 = new GreenfootImage("ant-with-food.gif");
     }
 
     /**
@@ -22,16 +28,54 @@ public class Ant extends Creature
      */
     public void act()
     {
-        randomWalk();
-        testFoodPile(); 
+        status();
     }
     
-    private void testFoodPile()
+    private boolean atHome()
+    {
+        if (getHomeHill() != null)
+        {
+            return (Math.abs(getX() - getHomeHill().getX()) < 4) &&
+                   (Math.abs(getY() - getHomeHill().getY()) < 4);
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    private void searchForFood()
+    {
+        randomWalk();
+        checkForFood();
+    }
+    
+    private void checkForFood()
     {
             Food food = (Food) getOneIntersectingObject(Food.class);
             if (food != null) 
             {
                 food.removeCrumb();
+                carryingFood = true;
+                setImage(image2);
             }
+    }
+    
+    private void status()
+    {
+        if (carryingFood == true)
+        {
+            walkTowardsHome();
+            if (atHome())
+            {
+                setImage(image1);
+                carryingFood = false;
+                getHomeHill().countFood();
+            }
+        }
+        else
+        {
+            searchForFood();
+        }
     }
 }
